cdeee13a18343bd1ff63ce32c6d126a0034e87b5
Andrew.Karpov <andrew.karpov@jetbrains.com>
2019-11-04 18:07:54 +0300

    Cleanup

---
+++ CSharpWorkshop/IssuesApp/Helpers/ActionProgress.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace IssuesApp.Helpers
+{
+  public class ActionProgress : IProgress<int>
+  {
+    private readonly Action<int> myOnProgress;
+
+    public ActionProgress(Action<int> onProgress)
+    {
+      myOnProgress = onProgress;
+    }
+
+    public void Report(int value)
+    {
+      myOnProgress(value);
+    }
+  }
+}
